4e6eacf800a603601cc868a8b57c1903d028cad3
AMANxSHI <amanjaiswal20584@acropolis.in>
2023-07-28 01:01:01 +0530

    ProjectCommit

---
+++ src/homeComponents/footer.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import "./style.css";
+class Footer extends Component {
+    render() {
+        return (
+            <footer >
+
+                
+                <div class="footer-copyright text-center py-3">© 2023 COPYRIGHT : ALGO VISUALIZER
+                    
+                </div>
+
+            </footer>
+        );
+    }
+}
+
+export default Footer;
