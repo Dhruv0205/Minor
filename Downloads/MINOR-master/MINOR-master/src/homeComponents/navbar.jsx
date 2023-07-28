6a8ab75cef3418b9030ac40da0f9c9641f2133c4
Dhruv0205 <dhruvsharma0505@gmail.com>
2023-07-28 11:52:14 +0530

    first commit

---
+++ Downloads/MINOR-master/MINOR-master/src/homeComponents/navbar.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav >
+                {/* <a className="navbar-brand" href="#">Algorithm Visualizer</a> */}
+                {/* <button > */}
+                    {/* <span className="navbar-toggler-icon"></span> */}
+                {/* </button> */}
+
+                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> }
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            {<a className="nav-link" href="#">About <span className="sr-only">(current)</span></a> }
+                        </li>
+
+                    </ul>
+                {</div> */}
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
